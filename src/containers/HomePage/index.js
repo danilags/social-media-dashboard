e3d467e099f8b903bb4421d84f0488583b9e1e3a
Daniel Sidabutar <aguspermadi94@gmail.com>
2018-07-10 01:59:48 +0700

    adding home page

---
+++ src/containers/HomePage/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import { Wrapper } from '../../components';
+
+class HomePage extends React.Component {
+  render() {
+    return (
+      <Wrapper>
+        <h3>Home Page</h3>
+      </Wrapper>
+    )
+  }
+}
+
+export default HomePage;
+
