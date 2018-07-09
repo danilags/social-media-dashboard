03d5f1c86e1baca077d4bee63b30bce48abbab46
Daniel Sidabutar <aguspermadi94@gmail.com>
2018-07-10 01:57:55 +0700

    structuring index app and wrapping the App route with Provider to pass the global props

--- src/index.js
+++ src/index.js
@@ -1,8 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import { Provider } from 'react-redux';
+import 'bootstrap/dist/css/bootstrap.css';
+import AppRoute from './containers';
+import store from './store'
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+<Provider store={store}>
+  <AppRoute />
+</Provider>, document.getElementById('root'));
 registerServiceWorker();
