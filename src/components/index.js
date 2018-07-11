5fb68d4a45dbd1853d954d388f3c17df85c5fc7b
Daniel Sidabutar <aguspermadi94@gmail.com>
2018-07-12 02:09:21 +0700

    importing file and adding new constants to define action api

--- src/components/index.js
+++ src/components/index.js
@@ -1,7 +1,10 @@
 import Wrapper from './Wrapper';
 import NavBar from './NavBar';
+import User from './User';
 
 export {
   Wrapper,
-  NavBar
+  NavBar,
+  User
 };
+
---
+++ src/constants/index.js
@@ -0,0 +1,6 @@
+export const API_BASE_URL ='https://jsonplaceholder.typicode.com/';
+
+export const GET_ALL_USERS = 'GET_ALL_USERS';
+export const GET_ALL_POSTS = 'GET_ALL_POSTS';
+export const GET_ALL_ALBUMS = 'GET_ALL_ALBUMS';
+export const GET_ALL_COMMENTS = 'GET_ALL_COMMENTS';
