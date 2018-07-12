cd848aae5a61bbdd4358d8ca18e3022c82552517
Daniel Sidabutar <aguspermadi94@gmail.com>
2018-07-13 00:28:23 +0700

    adding constant get comments

--- src/constants/index.js
+++ src/constants/index.js
@@ -3,4 +3,5 @@ export const API_BASE_URL ='https://jsonplaceholder.typicode.com/';
 export const GET_ALL_USERS = 'GET_ALL_USERS';
 export const GET_ALL_POSTS = 'GET_ALL_POSTS';
 export const GET_ALL_ALBUMS = 'GET_ALL_ALBUMS';
+export const GET_ALL_PHOTOS = 'GET_ALL_PHOTOS';
 export const GET_ALL_COMMENTS = 'GET_ALL_COMMENTS';
